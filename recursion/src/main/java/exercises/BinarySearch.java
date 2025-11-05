6c7f3cb69ae5e370cd351c435a255c38edc6c2e6
blerner5 <blerner5@student.touro.edu>
2025-11-04 19:25:45 -0500

    passed unit tests
    implemented recursion methods

--- recursion/src/main/java/exercises/BinarySearch.java
+++ recursion/src/main/java/exercises/BinarySearch.java
@@ -35,14 +35,10 @@ public class BinarySearch {
 
     // TODO: Implement recursively. DO NOT convert to a loop.
     private static int search(int[] a, int target, int lo, int hi) {
-        // Base case 1: empty range
-        // Base case 2: found at mid
-        // Recursive case: pick left or right half
-        // Hints:
-        //   int mid = lo + (hi - lo) / 2;
-        //   if (target < a[mid]) return search(a, target, lo, mid - 1);
-        //   else if (target > a[mid]) return search(a, target, mid + 1, hi);
-        //   else return mid;
-        return -2; // TEMP: replace with your recursive solution
+        if(lo>hi) return -1;
+        int mid = lo + (hi - lo) / 2;
+        if (a[mid] == target) return mid;
+        else if (target < a[mid]) return search(a, target, lo, mid - 1);
+        else return search(a, target, mid + 1, hi);
     }
 }
